49a5e1dabe70c7a66cd4ec6520e8c3e490cdff53
yuang.geng <yuang.geng@gmail.com>
2025-02-03 16:58:41 -0500

    Lasted code

--- Genetic_binning/Binning_genetic_New_traj.py
+++ Genetic_binning/Binning_genetic_New_traj.py
@@ -8,31 +8,79 @@ from scipy.stats import norm
 import pickle
 import sys
 from collections import Counter
+import pickle
+import random
+from random import sample, seed
+# Initializes seed
+seed(32611)
+
+###Define the fixed dataset
+
+# Loads data
+def reduce_data(initial_data, percent, total_size):
+    """Reduce the dataset by randomly sampling a given percentage."""
+
+    new_data = {
+        'RealTrajectories': [],
+        'EstTrajectories': [],
+        'Errors': []
+    }
+
+    if len(initial_data['RealTrajectories']) == 0:
+        return new_data
+    elif (total_size * percent) > (len(initial_data['RealTrajectories'])):
+        print('Not enough data. Decrease percent value.')
+        return new_data
+
+    # Randomly select indices
+    indices = random.sample(range(len(initial_data['RealTrajectories'])), int(total_size * percent))
+
+    # Extract selected data
+    for index in indices:
+        new_data['RealTrajectories'].append(initial_data['RealTrajectories'][index])
+        new_data['EstTrajectories'].append(initial_data['EstTrajectories'][index])
+        new_data['Errors'].append(initial_data['Errors'][index])
+
+    indices.sort()  # Sort indices to remove in reverse order
+    print("Selected Indices:", indices)
+
+    # Remove selected data from the original dataset
+    for index in reversed(indices):
+        initial_data['RealTrajectories'].pop(index)
+        initial_data['EstTrajectories'].pop(index)
+        initial_data['Errors'].pop(index)
+
+    return new_data
 
 
-def reduce_data(percent):
-    if percent != 1.0:
-        total_size = len(data['RealTrajectories'])
-        count = 0
-        while (float(total_size - count) / total_size) > percent:
-            index = int(np.random.uniform(0, len(data['RealTrajectories'])))
-            data['RealTrajectories'].pop(index)
-            data['EstTrajectories'].pop(index)
-            data['Errors'].pop(index)
-            count += 1
+# ###### old reduce_data function
+# def reduce_data(percent):
+#     if percent != 1.0:
+#         total_size = len(data['RealTrajectories'])
+#         count = 0
+#         while (float(total_size - count) / total_size) > percent:
+#             index = int(np.random.uniform(0, len(data['RealTrajectories'])))
+#             data['RealTrajectories'].pop(index)
+#             data['EstTrajectories'].pop(index)
+#             data['Errors'].pop(index)
+#             count += 1
 
 
 def split_data(bounds):
-    print(type(bounds))
-    full_bounds = [B_LEFT] + bounds.tolist() + [B_RIGHT]
-    #full_bounds = [B_LEFT] + bounds + [B_RIGHT]
+    if isinstance(bounds, np.ndarray):
+        full_bounds = [B_LEFT] + bounds.tolist() + [B_RIGHT]
+    elif isinstance(bounds, list):
+        full_bounds = [B_LEFT] + bounds + [B_RIGHT]
+    else:
+        raise TypeError("Bounds must be either a list or a numpy ndarray.")
+
     trajectories = []
     for i in range(num_regions):
         trajectories.append([])
 
-    for i in range(len(data['RealTrajectories'])):
-        cur_real_traj = data['RealTrajectories'][i]
-        cur_est_traj = data['EstTrajectories'][i]
+    for i in range(len(optimization_data['RealTrajectories'])):
+        cur_real_traj = optimization_data['RealTrajectories'][i]
+        cur_est_traj = optimization_data['EstTrajectories'][i]
         cur_real_traj.sort()
         cur_est_traj.sort()
 
@@ -63,78 +111,26 @@ def find_errors(bounds):
                     cur_max = trajectories[i][j][k][1]
                 size += 1
             temp_data.append(cur_max)
-        temp_data.append(100000)
+        temp_data.append(10000)
         temp_data.sort()
         region_data.append(temp_data)
         region_sizes.append(size)
 
     for i in range(num_regions):
-        index = int(np.ceil((len(region_data[i])) * 0.99) - 1)
+        index = int(np.ceil((len(region_data[i])) * (1-0.05/num_regions)) - 1)
         if len(region_data[i]) >= 1:
             errors.append(region_data[i][index])
         else:
-            errors.append(100000)
+            errors.append(10000)
 
     return errors, region_sizes
 
 
-##### Previous version #####
-# def split_data(B1, B2):
-#     bounds = [B0, B1, B2, B3]
-#     trajectories = [[],[],[]]
-#     for i in range(len(data['RealTrajectories'])):
-#         flag = -1
-#         for j in range(len(data['RealTrajectories'][i])):
-#             if flag == -1:
-#                 if j == 0:
-#                     temp_traj = [(data['RealTrajectories'][i][j], data['Errors'][i][j])]
-#                 if temp_traj[0][0] < bounds[1]:
-#                     flag = 1
-#                 elif temp_traj[0][0] < bounds[2]:
-#                     flag = 2
-#                 elif temp_traj[0][0] < bounds[3]:
-#                     flag = 3
-#             else:
-#                 if data['RealTrajectories'][i][j] <= bounds[flag] and data['RealTrajectories'][i][j] >= bounds[flag - 1]:
-#                     temp_traj.append((data['RealTrajectories'][i][j], data['Errors'][i][j]))
-#                     if j == len(data['RealTrajectories'][i]) - 1:
-#                         trajectories[flag - 1].append(temp_traj)
-#                 else:
-#                     trajectories[flag - 1].append(temp_traj)
-#                     flag = -1
-#                     temp_traj = [(data['RealTrajectories'][i][j], data['Errors'][i][j])]
-#     return trajectories
-#
-#
-# def find_errors(B1, B2):
-#     trajectories = split_data(B1, B2)
-#     region_data = [[],[],[]]
-#     errors = [0,0,0]
-#     region_size = [0,0,0]
-#     for i in range(3):
-#         temp_data = []
-#         for j in range(len(trajectories[i])):
-#             cur_max = 0
-#             for k in range(len(trajectories[i][j])):
-#                 if trajectories[i][j][k][1] > cur_max:
-#                     cur_max = trajectories[i][j][k][1]
-#                 region_size[i] += 1
-#             temp_data.append(cur_max)
-#         temp_data.append(10)
-#         temp_data.sort()
-#         region_data[i] = temp_data
-#     for i in range(3):
-#         index = int(np.ceil((len(region_data[i])) * 0.99) - 1)
-#         if len(region_data[i]) >= 1:
-#             errors[i] = region_data[i][index]
-#         else:
-#             errors[i] = 0
-#     return errors, region_size
+def partition_space(N):
+    X_min, X_max = -1.2, 0.6  # Fixed min and max bounds for partitioning
 
-
-
-def partition_space(X, N):
-    bounds = [(X.min(), X.max())] * (N - 1)  # Bounds for each region boundary
+    # Define bounds for each region boundary
+    bounds = [(X_min, X_max)] * (N - 1)
 
     # Differential Evolution Optimization
     result = differential_evolution(
@@ -147,10 +143,30 @@ def partition_space(X, N):
     )
 
     # Extract the optimized region boundaries
-    final_bounds = [X.min()] + list(result.x) + [X.max()]
+    final_bounds = [X_min] + list(result.x) + [X_max]
     regions = [(final_bounds[i], final_bounds[i + 1]) for i in range(N)]
 
     return regions
+
+#### Old version of X
+# def partition_space(X, N):
+#     bounds = [(X.min(), X.max())] * (N - 1)  # Bounds for each region boundary
+#
+#     # Differential Evolution Optimization
+#     result = differential_evolution(
+#         func=lambda region_bounds: objective(region_bounds, N),
+#         bounds=bounds,
+#         strategy='best1bin',
+#         maxiter=1000,
+#         tol=1e-7,
+#         disp=True
+#     )
+#
+#     # Extract the optimized region boundaries
+#     final_bounds = [X.min()] + list(result.x) + [X.max()]
+#     regions = [(final_bounds[i], final_bounds[i + 1]) for i in range(N)]
+#
+#     return regions
 
 
 # second version
@@ -158,13 +174,8 @@ def objective(region_bounds, N):
     # Check constraint: region_bounds must be in ascending order
     # If not, return a large penalty (here, 1e10)
     if any(region_bounds[i] >= region_bounds[i+1] for i in range(len(region_bounds)-1)):
-        return 1e10
-    # old version
-    # If boundaries are valid, continue with the evaluation
-    # bounds_numbers = find_errors(region_bounds[0], region_bounds[1])
-    # upper_bounds = bounds_numbers[0]
-    # number_traj = bounds_numbers[1]
-    # New version
+        return 9000000
+
     upper_bounds, number_traj = find_errors(region_bounds)
 
     total_weighted_penalty = 0
@@ -178,12 +189,22 @@ def objective(region_bounds, N):
     return total_weighted_penalty
 
 
-np.random.seed(1)
+def evaluate_loss(region_bounds, N):
+    upper_bounds, number_traj = find_errors(region_bounds)
 
-# data_percent = float(input('Enter percent of data to use (0.0,1.0): '))
-# num_regions = int(input('Enter number of regions: '))
-data_percent = 0.3
-num_regions = 3
+    total_weighted_penalty = 0
+    # Adjust the denominator if needed (using number_traj[0] three times seems unusual)
+    total = sum(number_traj)
+    for i in range(N):
+        weight = number_traj[i] / total
+        penalty = upper_bounds[i] * 10
+        total_weighted_penalty += weight * penalty
+
+    return total_weighted_penalty
+
+
+np.random.seed(32608)
+
 
 # Loads data
 try:
@@ -208,72 +229,78 @@ except:
     exit(1)
 
 # Finds edge bounds
-
-sorted_data = pos_data.copy()
-sorted_data.sort()
-B_LEFT = sorted_data[0]
-B_RIGHT = sorted_data[len(sorted_data) - 1]
+B_LEFT = -1.2
+B_RIGHT = 0.6
 print(f'{B_LEFT}, {B_RIGHT}')
 
+# Ensure `data` is defined before using it
+total_size = len(data['RealTrajectories'])
 
-reduce_data(data_percent) # change the dat
+# First reduction: Split data into two parts
+data_one = reduce_data(data, 0.5, total_size)
+data_two = reduce_data(data, 0.5, total_size)
 
-X = np.array(pos_data)  # Position data
-# Partition the space and get the regions
-regions = partition_space(X, num_regions)
-print('here are all the split regions:', regions)
-
-
-
-
+# Further split data_one
+data_one_size = len(data_one['RealTrajectories'])  # Fixed missing closing bracket
 
 
+output_file = "results_500data_95%.txt"
+optimization_data = reduce_data(data_one, 0.25, data_one_size)  # Adjust the data based on data_percent
+calculate_cp_data = reduce_data(data_one, 0.75, data_one_size)
 
+with open(output_file, "w") as f:
+    f.write(f"Data Percentage: {0.25}\n")  # Save data percentage
 
+    for num_regions in range(3, 8):  # Iterate from 3 to 7
+        # Partition the space into regions
+        regions = partition_space(num_regions)
+        regions_flat = [item for sublist in regions for item in (sublist if isinstance(sublist, tuple) else [sublist])]
 
-#############################################################################################################
-# Loads data
-try:
-    filename = 'binning_data/Thomas_all_data.pkl'
-    with open(filename, 'rb') as f:
-        data = pickle.load(f)
-except:
-    exit(1)
-try:
-    filename = 'binning_data/pos_data.pkl'
-    with open(filename, 'rb') as f:
-        pos_data = pickle.load(f)
-except:
-    exit(1)
-try:
-    filename = 'binning_data/error_data.pkl'
-    with open(filename, 'rb') as f:
-        error_data = pickle.load(f)
-except:
-    exit(1)
-# Finds edge bounds
-sorted_data = pos_data.copy()
-sorted_data.sort()
-B0 = sorted_data[0]
-B3 = sorted_data[len(sorted_data) - 1]
-print('here is the upper and lower bounds:', f'{B0}, {B3}')
+        # Compute the total loss
+        total_loss = evaluate_loss(regions_flat, num_regions)
+        cp_regions = find_errors()
+        # Save results to the file
+        f.write(f"  Number of Regions: {num_regions}\n")
+        f.write(f"  Revised Regions: {regions_flat}\n")
+        f.write(f"  Total Loss: {total_loss}\n\n")
+
+        # Print progress for debugging
+        print(f"Completed data_percent = {0.25}, num_regions = {num_regions}")
+        print(f"Revised Regions: {regions_flat}")
+        print(f"Total Loss: {total_loss}")
+
+print(f"Results saved to {output_file}.")
 
 
 
-
-
-
-
-# Run the global algorithm
-X = np.array(pos_data)  # Position data
-f_X = np.abs(np.array(error_data))
-N = 3  # Number of regions (you can change this based on your requirement)
-# Partition the space and get the regions
-regions = partition_space(X, f_X, N)
-print('here are all the split regions:', regions)
-all_bounds = np.array(regions).flatten()
-counts = Counter(all_bounds)
-two_bounds = [value for value, count in counts.items() if count > 1]
-errors_number_of_traj = find_errors(two_bounds[0], two_bounds[1])
-CP_bounds_99 = errors_number_of_traj[0]
-print(CP_bounds_99)
+# ###############################################################
+# ##### Test different ratio of the data for optimization #######
+# output_file = "results.txt"
+# data_percentages = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]  # Percentages to iterate over
+#
+# with open(output_file, "w") as f:
+#     for data_percent in data_percentages:  # Iterate over data percentages
+#         reduce_data(data_percent)  # Adjust the data based on data_percent
+#         f.write(f"Data Percentage: {data_percent}\n")  # Save data percentage
+#
+#         for num_regions in range(3, 8):  # Iterate from 3 to 7
+#             X = np.array(pos_data)  # Position data
+#
+#             # Partition the space into regions
+#             regions = partition_space(X, num_regions)
+#             regions_flat = [item for sublist in regions for item in (sublist if isinstance(sublist, tuple) else [sublist])]
+#
+#             # Compute the total loss
+#             total_loss = evaluate_loss(regions_flat, num_regions)
+#
+#             # Save results to the file
+#             f.write(f"  Number of Regions: {num_regions}\n")
+#             f.write(f"  Revised Regions: {regions_flat}\n")
+#             f.write(f"  Total Loss: {total_loss}\n\n")
+#
+#             # Print progress for debugging
+#             print(f"Completed data_percent = {data_percent}, num_regions = {num_regions}")
+#             print(f"Revised Regions: {regions_flat}")
+#             print(f"Total Loss: {total_loss}")
+#
+# print(f"Results saved to {output_file}.")
---
+++ baseline_approach/run_MC_baseline_with_fix_data.py
@@ -0,0 +1,341 @@
+
+
+import numpy as np
+# from gurobipy import *
+
+import gurobipy as gp
+from gurobipy import GRB
+
+import time
+import pickle
+import random
+import math
+import matplotlib.pyplot as plt
+from gurobipyTutorial import optimzeTimeAlphasKKT, optimzeTimeAlphasKKTNoMaxLowerBound, optimzeTimeAlphasKKTNoMaxLowerBoundMinArea
+
+
+import random
+from random import sample, seed
+# Initializes seed
+seed(32611)
+
+PLOT_VALIDATION_TRACES = True
+NUM_VALID_TO_PLOT = 100
+
+def computeRValuesIndiv(x_vals,y_vals,x_hats,y_hats):
+
+    R_vals = [ [math.sqrt((x_vals[i][j]-x_hats[i][j])**2 + (y_vals[i][j]-y_hats[i][j])**2) for j in range(len(x_vals[i]))]  for i in range(len(x_vals))  ]
+
+    return R_vals
+
+
+
+def computeCPCirlce(x_vals,y_vals,x_hats,y_hats,delta):
+
+    R_vals = [ math.sqrt((x_vals[i]-x_hats[i])**2 + (y_vals[i]-y_hats[i])**2) for i in range(len(x_vals))]
+
+    R_vals.sort()
+    R_vals.append(max(R_vals))
+
+    ind_to_ret = math.ceil(len(R_vals)*(1-delta))
+    return(R_vals[ind_to_ret])
+
+
+
+def computeCPFixedAlphas(x_vals,y_vals,x_hats,y_hats,alphas,delta):
+
+    R_vals = [max([alphas[j]*math.sqrt((x_vals[i][j]-x_hats[i][j])**2 + (y_vals[i][j]-y_hats[i][j])**2) for j in range(len(x_vals[i])) ]) for i in range(len(x_vals))]
+
+    R_vals.sort()
+    R_vals.append(max(R_vals))
+
+    ind_to_ret = math.ceil(len(R_vals)*(1-delta))
+    return R_vals[ind_to_ret]
+
+
+def computeCPFixedAlphas_MC(Rs, alphas, delta):
+    R_vals = [max([alphas[j] * Rs[i][j] for j in range(len(Rs[i]))]) for i in range(len(Rs))]
+
+    R_vals.sort()
+    R_vals.append(max(R_vals))
+
+    ind_to_ret = math.ceil(len(R_vals) * (1 - delta))
+    return R_vals[ind_to_ret]
+
+def computeCoverageRAndAlphas_MC(Rs,alphas,D_cp):
+    R_vals = [max([alphas[j] * Rs[i][j] for j in range(len(Rs[i]))]) for i in range(len(Rs))]
+    num_points_within = sum(r <= D_cp for r in R_vals)
+    coverage_pct = float(num_points_within)/len(R_vals)
+    return coverage_pct
+
+def computeCoverageRAndAlphas_MC_yuang(Rs,D_cp):
+    R_vals = [r for sublist in Rs for r in sublist]  # Flatten the list of lists
+    #R_vals = [max([alphas[j]*math.sqrt((x_vals[i][j]-x_hats[i][j])**2 + (y_vals[i][j]-y_hats[i][j])**2) for j in range(len(x_vals[i])) ]) for i in range(len(x_vals))]
+    num_points_within = sum(r <= D_cp for r in R_vals)
+    coverage_pct = float(num_points_within)/len(R_vals)
+    return coverage_pct
+
+def computeCoverageRAndAlphas(x_vals,y_vals,x_hats,y_hats,alphas,D_cp):
+
+    R_vals = [max([alphas[j]*math.sqrt((x_vals[i][j]-x_hats[i][j])**2 + (y_vals[i][j]-y_hats[i][j])**2) for j in range(len(x_vals[i])) ]) for i in range(len(x_vals))]
+
+    num_points_within = sum(r <= D_cp for r in R_vals)
+    coverage_pct = float(num_points_within)/len(R_vals)
+    return coverage_pct
+
+
+def computeCoverageCircle(x_vals,y_vals,x_hats,y_hats,Ds_cp):
+    coverage_count = 0
+    for i in range(len(x_vals)):
+
+        temp = sum([1 if math.sqrt((x_vals[i][j]-x_hats[i][j])**2 + (y_vals[i][j]-y_hats[i][j])**2) > Ds_cp[j] else 0 for j in range(len(x_vals[i]))])
+        if temp == 0:
+            coverage_count += 1
+        
+
+    coverage_pct = float(coverage_count)/len(x_vals)
+    return coverage_pct
+
+
+def plot_circle(x, y, size, color="-b", label=None):  # pragma: no cover
+    deg = list(range(0, 360, 5))
+    deg.append(0)
+    xl = [x + size * math.cos(np.deg2rad(d)) for d in deg]
+    yl = [y + size * math.sin(np.deg2rad(d)) for d in deg]
+
+    if label is None:
+        plt.plot(xl, yl, color)
+    else:
+        plt.plot(xl, yl, color,label=label)
+
+
+def reduce_data(initial_data, percent, total_size):
+    """Reduce the dataset by randomly sampling a given percentage."""
+
+    new_data = {
+        'RealTrajectories': [],
+        'EstTrajectories': [],
+        'Errors': []
+    }
+
+    if len(initial_data['RealTrajectories']) == 0:
+        return new_data
+    elif (total_size * percent) > (len(initial_data['RealTrajectories'])):
+        print('Not enough data. Decrease percent value.')
+        return new_data
+
+    # Randomly select indices
+    indices = random.sample(range(len(initial_data['RealTrajectories'])), int(total_size * percent))
+
+    # Extract selected data
+    for index in indices:
+        new_data['RealTrajectories'].append(initial_data['RealTrajectories'][index])
+        new_data['EstTrajectories'].append(initial_data['EstTrajectories'][index])
+        new_data['Errors'].append(initial_data['Errors'][index])
+
+    indices.sort()  # Sort indices to remove in reverse order
+    print("Selected Indices:", indices)
+
+    # Remove selected data from the original dataset
+    for index in reversed(indices):
+        initial_data['RealTrajectories'].pop(index)
+        initial_data['EstTrajectories'].pop(index)
+        initial_data['Errors'].pop(index)
+
+    return new_data
+
+
+if __name__ == "__main__":
+
+    try:
+        filename = 'data/data.pkl'
+        with open(filename, 'rb') as f:
+            data = pickle.load(f)
+    except:
+        exit(1)
+
+    try:
+        filename = 'data/pos_data.pkl'
+        with open(filename, 'rb') as f:
+            pos_data = pickle.load(f)
+    except:
+        exit(1)
+    try:
+        filename = 'data/error_data.pkl'
+        with open(filename, 'rb') as f:
+            error_data = pickle.load(f)
+        error_data = [abs(error) for error in error_data]
+    except:
+        exit(1)
+
+    sorted_data = pos_data.copy()
+    sorted_data.sort()
+    B_LEFT = sorted_data[0]
+    B_RIGHT = sorted_data[len(sorted_data) - 1]
+    print(f'{B_LEFT}, {B_RIGHT}')
+
+    # Ensure `data` is defined before using it
+    total_size = len(data['RealTrajectories'])
+
+    # First reduction: Split data into two parts
+    data_one = reduce_data(data, 0.5, total_size)
+    data_two = reduce_data(data, 0.5, total_size)
+
+    # Further split data_one
+    data_one_size = len(data_one['RealTrajectories'])  # Fixed missing closing bracket
+    alpha_data = reduce_data(data_two, 0.2, data_one_size)
+    bounds_data = reduce_data(data_two, 0.8, data_one_size)
+
+    alpha_disp = alpha_data['Errors']
+    cp_caculate_disp = bounds_data['Errors']
+
+
+    p_len = 90
+    delta = 0.01
+    R_vals_calib_alpha_MC = alpha_disp
+    R_vals_calib_alpha_MC = [
+        sublist[:p_len] for sublist in R_vals_calib_alpha_MC
+    ]
+    ## run optimziation
+    #M = 100000  # big value for linearization of max constraint
+    M = 100000
+    start_time = time.time()
+    m = optimzeTimeAlphasKKTNoMaxLowerBound(R_vals_calib_alpha_MC, delta, M)
+    end_time = time.time()
+
+    start_time_milp = time.time()
+    m_milp = optimzeTimeAlphasKKT(R_vals_calib_alpha_MC, delta, M)
+    end_time_milp = time.time()
+
+    print("Solve time: " + str(end_time - start_time))
+    print("Solve time MILP: " + str(end_time_milp - start_time_milp))
+    # print("Solve time min area: " + str(end_time_min_area-start_time_min_area))
+
+    alphas = []
+    for v in m.getVars():
+        if "alphas" in v.varName:
+            alphas.append(v.x)
+        if "q" in v.varName:
+            # print(v.x)
+            print("obj: " + str(v.x))
+
+    alphas_milp = []
+    for v in m_milp.getVars():
+        if "alphas" in v.varName:
+            alphas_milp.append(v.x)
+        if "q" in v.varName:
+            # print(v.x)
+            print("obj MILP: " + str(v.x))
+
+    print("alphas: " + str(alphas))
+    print("alphas MILP: " + str(alphas_milp))
+
+    plt.plot(alphas, 'k*')
+    plt.xlabel("Prediction Horizon", fontsize="16")
+    plt.ylabel("Alpha Value", fontsize="16")
+
+    plt.savefig('images/forPaper/alphas_MC.png')
+
+
+    ## run CP using alpha values on remaining calibration data
+    calibrate_R = cp_caculate_disp
+    calibrate_R = [
+        sublist[:p_len] for sublist in calibrate_R
+    ]
+    D_cp = computeCPFixedAlphas_MC(calibrate_R,alphas,delta)
+    print("The final confromal bounds: ",D_cp)
+
+    # validate_R = discrepancy_data[700:1400]
+    # validate_R = [
+    #     sublist[:p_len] for sublist in validate_R
+    # ]
+    # validation_coverage_MC = computeCoverageRAndAlphas_MC(validate_R,alphas,D_cp)
+    # print("Their validation coverage MC: " + str(validation_coverage_MC))
+    #
+    #
+    # validation_coverage_MC1 = computeCoverageRAndAlphas_MC_yuang(validate_R,D_cp)
+    # print("Real validation coverage MC: " + str(validation_coverage_MC1))
+    #
+    #
+    #
+
+
+
+    ###########################
+    discrepancy_data = data['Errors']
+    p_len = 90
+    delta = 0.01
+    trace_for_alphas = 50 ## 100 trajectories for alphas
+    makePlots = True
+    random.seed(34956)
+
+    #1. We get 100 trajectories to calculate the R values
+    # R_vals_calib_alpha = computeRValuesIndiv(all_x_calib_alphas, all_y_calib_alphas, all_x_l_calib_alphas,
+    #                                          all_y_l_calib_alphas)
+    R_vals_calib_alpha_MC = discrepancy_data[0:trace_for_alphas]
+    R_vals_calib_alpha_MC = [
+        sublist[:p_len] for sublist in R_vals_calib_alpha_MC
+    ]
+    ## run optimziation
+    M = 100000  # big value for linearization of max constraint
+
+    start_time = time.time()
+    m = optimzeTimeAlphasKKTNoMaxLowerBound(R_vals_calib_alpha_MC, delta, M)
+    end_time = time.time()
+
+    start_time_milp = time.time()
+    m_milp = optimzeTimeAlphasKKT(R_vals_calib_alpha_MC, delta, M)
+    end_time_milp = time.time()
+
+    # start_time_min_area = time.time()
+    # m_min_area = optimzeTimeAlphasKKTNoMaxLowerBoundMinArea(R_vals_calib_alpha,delta,M)
+    # end_time_min_area = time.time()
+
+    print("Solve time: " + str(end_time - start_time))
+    print("Solve time MILP: " + str(end_time_milp - start_time_milp))
+    # print("Solve time min area: " + str(end_time_min_area-start_time_min_area))
+
+    alphas = []
+    for v in m.getVars():
+        if "alphas" in v.varName:
+            alphas.append(v.x)
+        if "q" in v.varName:
+            # print(v.x)
+            print("obj: " + str(v.x))
+
+    alphas_milp = []
+    for v in m_milp.getVars():
+        if "alphas" in v.varName:
+            alphas_milp.append(v.x)
+        if "q" in v.varName:
+            # print(v.x)
+            print("obj MILP: " + str(v.x))
+
+    print("alphas: " + str(alphas))
+    print("alphas MILP: " + str(alphas_milp))
+
+    plt.plot(alphas, 'k*')
+    plt.xlabel("Prediction Horizon", fontsize="16")
+    plt.ylabel("Alpha Value", fontsize="16")
+
+    plt.savefig('images/forPaper/alphas_MC.png')
+
+
+    ## run CP using alpha values on remaining calibration data
+    calibrate_R = discrepancy_data[trace_for_alphas:700]
+    calibrate_R = [
+        sublist[:p_len] for sublist in calibrate_R
+    ]
+    D_cp = computeCPFixedAlphas_MC(calibrate_R,alphas,delta)
+
+    validate_R = discrepancy_data[700:1400]
+    validate_R = [
+        sublist[:p_len] for sublist in validate_R
+    ]
+    validation_coverage_MC = computeCoverageRAndAlphas_MC(validate_R,alphas,D_cp)
+    print("Their validation coverage MC: " + str(validation_coverage_MC))
+
+
+    validation_coverage_MC1 = computeCoverageRAndAlphas_MC_yuang(validate_R,D_cp)
+    print("Real validation coverage MC: " + str(validation_coverage_MC1))
+
